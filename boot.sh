03a86cea94e0959aa6a72a8861d6cefac99010f7
Uckermark <93996544+Uckermark@users.noreply.github.com>
2023-01-04 00:57:38 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,4 +1,11 @@
 set -e
+
+if [ "$1" = "-v" ]; then
+    out=/dev/stdout
+else
+    out=/dev/null
+fi
+
 os=$(uname)
 if [ "$os" = "Linux" ]; then
     bin="$(pwd)"/bin/"$os"/"$(uname -m)"
@@ -8,12 +15,12 @@ fi
 boot="$(pwd)"/boot
 
 echo "[*] Pwning device..."
-"$bin"/gaster pwn
-"$bin"/gaster reset
+"$bin"/gaster pwn 2&>1 "$out" 
+"$bin"/gaster reset 2&>1 "$out"
 
 echo "[*] Booting device..."
-"$bin"/irecovery -f "$boot"/iBSS.img4
-"$bin"/irecovery -f "$boot"/ibot.img4
+"$bin"/irecovery -f "$boot"/iBSS.img4 2&>1 "$out"
+"$bin"/irecovery -f "$boot"/ibot.img4 2&>1 "$out"
 
 echo "[*] Done!"
 echo "    The device should now boot to iOS"
